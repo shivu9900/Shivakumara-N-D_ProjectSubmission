2f1c7a5e980f0d828682a4b5f4eba5c92c8e05a4
shivu9900 <34368420+shivu9900@users.noreply.github.com>
2025-07-25 21:21:23 +0530

    terraform code to create security groups

---
+++ terraform codes/security_groups.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg"
+  description = "Allow SSH from specific IP"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["167.103.54.90/32"]  # Your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-sg"
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Allow all traffic within VPC"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.100.0.0/16"]  # VPC CIDR
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-sg"
+  }
+}
