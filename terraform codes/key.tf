77728713339c5b11c3dbe238bb250735b64e519a
shivu9900 <34368420+shivu9900@users.noreply.github.com>
2025-07-25 21:19:56 +0530

    terraform code to create ssh key

---
+++ terraform codes/key.tf
@@ -0,0 +1,14 @@
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "upgrad-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
